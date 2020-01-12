897eaa90ba93128a4d086f245edda6a31aba4d03
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-12 15:18:02 +0900

    교통카드

---
+++ 15.4.py
@@ -0,0 +1,12 @@
+age=int(input());
+total=9000;
+
+if 7<= age <=12:
+	total=total-650;
+	print(total);
+elif 12<= age <=18:
+		total=total-1050;
+		print(total);
+elif age >=19:
+		total=total-1250;
+		print(total);
