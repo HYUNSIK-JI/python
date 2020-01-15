69cf8034dc756e950f553061315726660b69c01e
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-15 16:44:17 +0900

    특정단어 개수 세기

---
+++ 24.5.py
@@ -0,0 +1,4 @@
+sentence="the grown-ups response, this time, was to advise me to lay aside my drawings of boaconstrictors,whether from the inside or the outside, and devote myself instead to geography,history,arithmetic,and grammer. That is why, at the,age of six, I gave up what might have been a magnificent career as a painter. I had been disheartened by the failure of my Drawing Number One and my Drawing Number Two. Grown-ups never understand anything by themselves and it is tiresome for children to be always and forever explaining things to the"
+x=sentence.strip(',.');
+y=x.count(' the');
+print(y);
