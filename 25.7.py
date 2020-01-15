6dbee6d71ec0a3959c9d56d0abb90b603df67443
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-16 02:00:47 +0900

    평균점수 구하기

---
+++ 25.7.py
@@ -0,0 +1,7 @@
+maria={'korean':94,'english':91,'mathematics':89,'science':83};
+size=len(maria.keys());
+total=0;
+for value in maria.values():
+	total+=value;
+average=(total/size);
+print(average);
