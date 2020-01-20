a9f9f0e4a718a42992489a0cc52d65ba5ac91b20
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-20 16:03:33 +0900

    가장낮은점수,가장높은점수,평균점수 구하는 함수만들기

--- 30.7.py
+++ 30.7.py
@@ -1,7 +1,15 @@
 korean,english,mathematics,science=map(int,input().split());
 def get_score(*agrs):
-	average=((korean+english+mathematics+science)/4);
-	return min(agrs),max(agrs),average
-min_score,max_score,average_score=get_score(korean,english,mathematics,science);
+	return min(agrs),max(agrs)
+def get_average_score(**kwagrs):
+	return sum(kwagrs.values())/len(kwagrs);
+
+min_score,max_score=get_score(korean,english,mathematics,science);
+average_score=get_average_score(korean=korean,english=english,mathematics=mathematics,science=science);
 
 print('낮은점수:',min_score,'높은점수:',max_score,'평균점수:',average_score)
+
+min_score,max_score=get_score(english,science);
+average_score=get_average_score(english=english,science=science);
+
+print('낮은점수:',min_score,'높은점수:',max_score,'평균점수:',average_score);
