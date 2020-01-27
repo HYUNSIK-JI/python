bf7dcc7fd5775ed8d7f50530d97af5af06469bf0
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-27 15:05:57 +0900

    피타고라스의정리로 두점거리구하기

---
+++ 37.1.2.py
@@ -0,0 +1,14 @@
+import math
+
+class point2D:
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+p1=point2D(x=30,y=20)
+p2=point2D(x=60,y=50)
+
+a=p2.x-p1.x
+b=p2.y-p1.y
+
+c=math.sqrt((a*a)+(b*b))
+print(c)
