cdfccddffc373d6e86aeef73a7b438ab4b9b5e64
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-01-30 15:08:00 +0900

    코루틴을 종료하고 예외 처리하기

---
+++ 41.3.py
@@ -0,0 +1,13 @@
+def number_coroutine():
+	try:
+		while True:
+			x=(yield)
+			print(x,end=' ')
+	except GeneratorExit:
+		print()
+		print('코루틴 종료')
+co=number_coroutine()
+next(co)
+for i in range(20):
+	print(co.send(i))
+co.close()
